5f8af0e0e5b7bc9a4b34b1984519baafaf86a1ea
Parth Pawar <parth290601@rediffmail.com>
2022-06-22 18:22:13 +0530

    Interval overlapping BIT + Added fast hashmap gp hash table

--- CSES/Sorting and Searching/NestedRangesCount.cpp
+++ CSES/Sorting and Searching/NestedRangesCount.cpp
@@ -2,9 +2,28 @@
 using namespace std;
 //#pragma GCC optimize "trapv"
 #define ll long long
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+#pragma GCC optimize("O3")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,fma") 
+#pragma GCC optimize("unroll-loops")
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
 
 //Very nice explanation: https://youtu.be/ORdmSir
-map<int,int> compress;
+gp_hash_table<int,int, custom_hash> compress;
 
 struct interval {
     int l,r,idx;
