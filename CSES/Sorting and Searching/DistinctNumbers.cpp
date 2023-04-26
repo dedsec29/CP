fdd04e5843311f65623e9bfeba1fe89236d6b537
Parth Pawar <parth290601@rediffmail.com>
2023-04-26 23:33:09 +0530

    std::unique for erasing duplicates with least constant factor

--- CSES/Sorting and Searching/DistinctNumbers.cpp
+++ CSES/Sorting and Searching/DistinctNumbers.cpp
@@ -5,12 +5,10 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n; cin>>n;
-    unordered_map<int,int> mp;
-    mp.reserve(n);
-    for (int i=0;i<n;i++) {
-    	int r; cin>>r;
-    	mp[r]++;
-    }
-    cout<<mp.size();
+    vector<int> v(n);
+    for (int& i: v) cin >> i;
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    cout << v.size() << "\n";
     return 0;
 }
